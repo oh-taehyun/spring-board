aeef7513852c2e0ff443147337eb78451150e19b
Tae hyun <xogus7038@naver.com>
2024-05-14 01:21:55 +0900

    feat : 로그인 및 회원가입 기능 추가
    Related to: #10

---
+++ src/main/java/org/example/springboard/repository/UserAccountRepository.java
@@ -0,0 +1,10 @@
+package org.example.springboard.repository;
+
+import org.example.springboard.entity.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount,Long> {
+    Optional<UserAccount> findByUserId(String userId);
+}
